2c6ea25a83db8437d507d49fec59f9dbdfb5dc66
unknown <goodiswill2016@gmail.com>
2018-04-27 23:41:06 -0700

    04272018

--- app/src/main/java/com/jiusite/pos/main/AppStartActivity.java
+++ app/src/main/java/com/jiusite/pos/main/AppStartActivity.java
@@ -1,8 +1,10 @@
 package com.jiusite.pos.main;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Display;
 
 import com.jiusite.pos.R;
 
@@ -12,7 +14,15 @@ public class AppStartActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appstart);
-		
+
+        //init Func
+        Context context = getApplicationContext();
+        //Func.setContext(context);
+
+        //init screen
+        Display display = getWindowManager().getDefaultDisplay();
+        //Func.initScreen(display);
+
 		Intent intent = new Intent(AppStartActivity.this, MainActivity.class);
 		startActivity(intent);
 		
